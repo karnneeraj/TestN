8ac7251e3057090925056bb66969b2819e9a2e3d
karnneeraj <50608214+karnneeraj@users.noreply.github.com>
2019-06-14 09:38:15 +0530

    Update ATT.java

--- web driver demo/src/seleniumwebdemo/ATT.java
+++ web driver demo/src/seleniumwebdemo/ATT.java
@@ -1,21 +1,4 @@
-package seleniumwebdemo;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class ATT {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\NEERAJKUMAR\\Desktop\\selenium\\geckodriver-v0.24.0-win64\\geckodriver.exe");
-	     WebDriver driver = new FirefoxDriver();
-	     driver.get("http://efmsms-uat.hydc.sbc.com:8008/ms");
-	     driver.manage().window().maximize();
-	     driver.findElement(By.id("ATTUID")).sendKeys("nk8289");
-	     driver.findElement(By.id("Password")).sendKeys("Bang07Lore!!");
 	     
 	     
 	}
